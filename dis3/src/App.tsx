8a6f99cb0165036dc5e9f355d9f6224a2400352f
jianying liu <jianying@bu.edu>
2025-02-17 14:43:11 -0500

    dis3

---
+++ dis3/src/App.tsx
@@ -0,0 +1,11 @@
+import ArtworksListContent from './components/artworks-list-content'
+import './App.css'
+
+
+function App() {
+  return (
+    <ArtworksListContent />
+  )
+}
+
+export default App
